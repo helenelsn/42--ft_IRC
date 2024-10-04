c55628256fe250d909b9cbc91b4f8baf025898de
PapeFrancois <119733633+PapeFrancois@users.noreply.github.com>
2024-10-05 01:17:03 +0200

    implementation de la classe channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -10,41 +10,53 @@
 // /*                                                                            */
 // /* ************************************************************************** */
 
-// #ifndef CHANNEL_HPP
-// # define CHANNEL_HPP
+#pragma once
 
-// # include "irc.hpp"
-// # include "Client.hpp"
+#include "irc.hpp"
+#include "Client.hpp"
 
-// class Channel
-// {
-//     private:
-//         typedef std::map<std::string, Client> members;
-//         typedef std::map<std::string, Client> operators;
-//         typedef std::vector<std::string> usersInvited;
+class Channel
+{
+    private:
+        typedef std::map<std::string, Client> members;
+        typedef std::map<std::string, Client> operators;
+        typedef std::map<std::string, Client> invitedUsers;
         
-// 		members			members_;
-// 		operators		operators_;
-//         std::string     name_;
-//         std::string     topic_;
-//         bool            topicRestrictionMode_;
-//         bool            inviteOnlyMode_;
-// 		usersInvited	usersInvited_;
-//         bool            passwordMode_;
-//         std::string     password_;
-//         bool            userLimitMode_;
-//         unsigned int    userLimit_;
+		members			_members;
+		operators		_operators;
+        std::string     _name;
+        std::string     _topic;
+        bool            _topicRestrictionMode;
+        bool            _inviteOnlyMode;
+		invitedUsers	_invitedUsers;
+        bool            _passwordMode;
+        std::string     _password;
+        bool            _userLimitMode;
+        unsigned int    _userLimit;
         
-//     public:
-//         Channel();
-//         Channel (const Channel& other);
-//         ~Channel();
+    public:
+        Channel();
+		Channel(const std::string& name, Client& client);
+        Channel (const Channel& other);
+        ~Channel();
 
-//         Channel& operator = (const Channel& other);
+        Channel& operator = (const Channel& other);
 
-        
-
-    
-// };
-
-// #endif
+		bool				isMember(const std::string& nick);
+        Client&				getMember(const std::string& nick);
+		unsigned int		getNumberOfMembers();
+		bool				isOperator(const std::string& nick);
+		Client& 			getOperator(const std::string& nick);
+		unsigned int		getNumberOfOperators();
+		bool				isInvited(const std::string& nick);
+		Client&				getInvitedUsers(const std::string& nick);
+		unsigned int		getNumberOfInvitedUsers();
+		const std::string&	getName();
+		const std::string&	getTopic();
+		const bool&			getTopicRestrictionMode();
+		const bool&			getInviteOnlyMode();
+		const bool&			getPasswordMode();
+		const std::string&	getPassword();
+		const bool&			getUserLimitMode();
+		const unsigned int&	getUserLimit();
+};
--- sources/Channel.cpp
+++ sources/Channel.cpp
@@ -3,12 +3,183 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Helene <Helene@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 14:51:52 by Helene            #+#    #+#             */
-/*   Updated: 2024/09/22 18:29:39 by Helene           ###   ########.fr       */
+/*   Updated: 2024/10/05 01:16:18 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Channel.hpp"
 
+Channel::Channel() : _topic(""), _topicRestrictionMode(0), _inviteOnlyMode(0),
+	_passwordMode(0), _userLimitMode(0) {}
+
+Channel::Channel(const Channel& other)
+{
+	this->_members = other._members;
+	this->_operators = other._operators;
+	this->_name = other._name;
+	this->_topic = other._topic;
+	this->_topicRestrictionMode = other._topicRestrictionMode;
+	this->_inviteOnlyMode = other._inviteOnlyMode;
+	this->_invitedUsers = other._invitedUsers;
+	this->_passwordMode = other._passwordMode;
+	this->_password = other._password;
+	this->_userLimitMode = other._userLimitMode;
+	this->_userLimit = other._userLimit;
+}
+
+Channel::Channel(const std::string& name, Client& member) : _topic(""),
+	_topicRestrictionMode(0), _inviteOnlyMode(0), _passwordMode(0),
+	_userLimitMode(0)
+{
+	this->_name = name;
+	this->_members[member.getNickname()] = member;
+	this->_operators[member.getNickname()] = member;
+}
+
+Channel::~Channel() {}
+
+Channel& Channel::operator = (const Channel& other)
+{
+	this->_members = other._members;
+	this->_operators = other._operators;
+	this->_name = other._name;
+	this->_topic = other._topic;
+	this->_topicRestrictionMode = other._topicRestrictionMode;
+	this->_inviteOnlyMode = other._inviteOnlyMode;
+	this->_invitedUsers = other._invitedUsers;
+	this->_passwordMode = other._passwordMode;
+	this->_password = other._password;
+	this->_userLimitMode = other._userLimitMode;
+	this->_userLimit = other._userLimit;
+	return (*this);
+}
+
+bool Channel::isMember(const std::string& nick)
+{
+	std::map<std::string, Client>::const_iterator it;
+
+	it = this->_members.find(nick);
+	if (it != this->_members.end())
+		return true;
+	return false;
+}
+
+Client& Channel::getMember(const std::string& nick)
+{
+	std::map<std::string, Client>::iterator it;
+
+	it = this->_members.find(nick);
+	return it->second;
+}
+
+unsigned int Channel::getNumberOfMembers()
+{
+	std::map<std::string, Client>::iterator	it;
+	int										n;
+
+	n = 0;
+	for (it = this->_members.begin(); it != this->_members.end(); it++)
+		n++;
+	return n;
+}
+
+bool Channel::isOperator(const std::string& nick)
+{
+	std::map<std::string, Client>::const_iterator it;
+
+	it = this->_operators.find(nick);
+	if (it != this->_operators.end())
+		return true;
+	return false;
+}
+
+Client& Channel::getOperator(const std::string& nick)
+{
+	std::map<std::string, Client>::iterator it;
+
+	it = this->_operators.find(nick);
+	return it->second;
+}
+
+unsigned int Channel::getNumberOfOperators()
+{
+	std::map<std::string, Client>::iterator	it;
+	int										n;
+
+	n = 0;
+	for (it = this->_operators.begin(); it != this->_operators.end(); it++)
+		n++;
+	return n;
+}
+
+bool Channel::isInvited(const std::string& nick)
+{
+	std::map<std::string, Client>::const_iterator it;
+
+	it = this->_invitedUsers.find(nick);
+	if (it != this->_invitedUsers.end())
+		return true;
+	return false;
+}
+
+Client& Channel::getInvitedUsers(const std::string& nick)
+{
+	std::map<std::string, Client>::iterator it;
+
+	it = this->_invitedUsers.find(nick);
+	return it->second;
+}
+
+unsigned int Channel::getNumberOfInvitedUsers()
+{
+	std::map<std::string, Client>::iterator	it;
+	int										n;
+
+	n = 0;
+	for (it = this->_invitedUsers.begin(); it != this->_invitedUsers.end(); it++)
+		n++;
+	return n;
+}
+
+const std::string& Channel::getName()
+{
+	return this->_name;
+}
+
+const std::string& Channel::getTopic()
+{
+	return this->_topic;
+}
+
+const bool& Channel::getTopicRestrictionMode()
+{
+	return this->_topicRestrictionMode;
+}
+
+const bool& Channel::getInviteOnlyMode()
+{
+	return this->_inviteOnlyMode;
+}
+
+const bool& Channel::getPasswordMode()
+{
+	return this->_passwordMode;
+}
+
+const std::string& Channel::getPassword()
+{
+	return this->_password;
+}
+
+const bool& Channel::getUserLimitMode()
+{
+	return this->_userLimitMode;
+}
+
+const unsigned int& Channel::getUserLimit()
+{
+	return this->_userLimit;
+}
